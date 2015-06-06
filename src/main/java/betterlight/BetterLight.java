fe140b2a8765b7f37a75870a3fe0e8fc4cbb16ba
Jiahao Li <isundaylee.reg@gmail.com>
2015-06-06 16:13:06 -0700

    Add the source.

---
+++ src/main/java/betterlight/BetterLight.java
@@ -0,0 +1,18 @@
+package betterlight;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BetterLight.MODID, version = BetterLight.VERSION)
+public class BetterLight
+{
+    public static final String MODID = "betterlight";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
